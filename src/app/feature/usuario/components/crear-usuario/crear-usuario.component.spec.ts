86fd54d4166005cfa6b65382d5d8481fa25c589f
JuanHurtado95 <juan.carlos.hurtado@correounivalle.edu.co>
2021-10-13 09:20:54 -0500

    Commit toast

--- src/app/feature/usuario/components/crear-usuario/crear-usuario.component.spec.ts
+++ src/app/feature/usuario/components/crear-usuario/crear-usuario.component.spec.ts
@@ -1,14 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpService } from '@core/services/http.service';
+import { UsuarioService } from '@usuario/shared/service/usuario.service';
+import { of } from 'rxjs';
 
 import { CrearUsuarioComponent } from './crear-usuario.component';
 
-describe('AddComponent', () => {
+describe('CrearUsuarioComponent', () => {
   let component: CrearUsuarioComponent;
   let fixture: ComponentFixture<CrearUsuarioComponent>;
+  let usuarioService: UsuarioService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CrearUsuarioComponent ]
+      declarations: [ CrearUsuarioComponent ],
+      imports: [
+        CommonModule,
+        HttpClientModule,
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule
+      ],
+      providers: [UsuarioService, HttpService],
     })
     .compileComponents();
   }));
@@ -16,10 +32,31 @@ describe('AddComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CrearUsuarioComponent);
     component = fixture.componentInstance;
+    usuarioService = TestBed.inject(UsuarioService);
+    spyOn(usuarioService, 'createUsuario').and.returnValue(
+        of(true)
+      );
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Usuario invalido cuando Cedula vacia', () => {
+    expect(component.usuario.cedula).toBeFalsy();
+  });
+
+  it('Usuario invalido cuando nombre vacio', () => {
+    expect(component.usuario.nombre).toBeFalsy();
+  });
+
+  it('Usuario invalido cuando telefono vacio', () => {
+    expect(component.usuario.telefono).toBeFalsy();
+  });
+
+  it('Creando Usuario', async () => {
+    
+  });
+
 });
